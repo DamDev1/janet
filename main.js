985a636d9a44eb58ba2f74b0ba6f9fc8b08f0cbd
SadCoder-01 <fridaydamilare22@gmail.com>
2021-07-15 23:55:25 +0100

    styling and adding some file's

--- main.js
+++ main.js
@@ -1,15 +1,41 @@
 const sideBar = document.querySelector(".sideBar");
 const toggle = document.querySelector(".toggle");
+const closetoggle = document.querySelector(".closetoggle");
+
+const tabsButton = document.querySelectorAll(".tabsButton");
+const tabContent = document.querySelectorAll(".tabContent");
 
 sideBar.style.display = "none";
+
 
 
 toggle.addEventListener("click", function(e){
     if(sideBar.style.display == "none"){
         sideBar.style.display = "block";
-    }else{
+    }
+    else{
         sideBar.style.display = "none";
     }
 
     e.preventDefault();
-})
+});
+
+
+closetoggle.addEventListener("click", function(){
+    sideBar.style.display = "none";
+});
+
+
+function showTabs(tabsIndex, colorCode){
+    tabsButton.forEach(function(show){
+        show.style.background = "";
+    });
+    tabsButton[tabsIndex].style.background = "green";
+    tabsButton[tabsIndex].style.color = "white";
+
+    tabContent.forEach(function(show){
+        show.style.display = "none"
+    });
+    tabContent[tabsIndex].style.display = "block";
+}
+showTabs(0);
